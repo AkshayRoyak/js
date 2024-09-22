be9c8f70524e7c1056ceaae07a14303b22678801
AkshayRoyak <work.akshayroy@gmail.com>
2024-09-22 05:56:35 +0000

    conversion od dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score="33"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber= Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+let abc="33abc"
+
+let valueInNum= Number(abc)
+console.log(typeof valueInNum)
+console.log(valueInNum)
+
+// "33"=> 33
+// "33abc" => NaN
+// true=> 1  ;  false=> 0
+
+let isLogined=1;
+let booleanisLoggedIn=Boolean(isLogined)
+console.log(booleanisLoggedIn)
+
+// 1=> true
+// 0=> false
+// " "=> false
+// "Akshay"=> true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+// 33 this is string
+console.log(typeof stringNumber)
+// string
