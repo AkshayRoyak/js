9d3d90d409b40302db5b56db7c99524604679f51
AkshayRoyak <work.akshayroy@gmail.com>
2024-09-22 05:29:58 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"  // treat all js code as newer version
+
+//alert(3+3); // wew are using node js not browser so we see error in this case
+
+console.log(3+3)
+/* semi-colons is not cumpulsory just write in next line and
+also try to make code more redable*/
+
+console.log("Akshay")
+
+let name="Akshay"
+let age=18
+let isLogined=false
+let state;
+
+//number=> 2 to power 53
+//bigint
+//string=""
+//boolean=> true/false
+//null => standalone value
+// undefined => 
+// symbol => to find unique
+
+//object
+console.log(typeof("Akshay"));
+console.table([typeof name,typeof age,typeof isLogined,typeof state])
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) // object
