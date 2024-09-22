141e80459fe37a3a3dbac6264604176c4216eed3
AkshayRoyak <work.akshayroy@gmail.com>
2024-09-22 06:19:36 +0000

    conversion of dataTypes 2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,35 +1,74 @@
-let score="33"
+// let score="33"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber= Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber= Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
-let abc="33abc"
+// let abc="33abc"
 
-let valueInNum= Number(abc)
-console.log(typeof valueInNum)
-console.log(valueInNum)
+// let valueInNum= Number(abc)
+// console.log(typeof valueInNum)
+// console.log(valueInNum)
 
-// "33"=> 33
-// "33abc" => NaN
-// true=> 1  ;  false=> 0
+// // "33"=> 33
+// // "33abc" => NaN
+// // true=> 1  ;  false=> 0
 
-let isLogined=1;
-let booleanisLoggedIn=Boolean(isLogined)
-console.log(booleanisLoggedIn)
+// let isLogined=1;
+// let booleanisLoggedIn=Boolean(isLogined)
+// console.log(booleanisLoggedIn)
 
-// 1=> true
-// 0=> false
-// " "=> false
-// "Akshay"=> true
+// // 1=> true
+// // 0=> false
+// // " "=> false
+// // "Akshay"=> true
 
-let someNumber=33
-let stringNumber=String(someNumber)
-console.log(stringNumber)
-// 33 this is string
-console.log(typeof stringNumber)
-// string
+// let someNumber=33
+// let stringNumber=String(someNumber)
+// console.log(stringNumber)
+// // 33 this is string
+// console.log(typeof stringNumber)
+// // string
+
+
+//**************Operations********
+let value=3
+let negValue= -value
+//console.log(negValue)
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(5%2)
+
+
+// let str1="Akshay"
+// let str2=" Roy"
+// let str3= str1 + str2
+// console.log(str3)
+
+
+// console.log("1"+2)     //12
+// console.log(1+"2")     //12
+// console.log("1"+2+2)   //122
+// console.log(1+2+"2")   //32
+
+
+// console.log(true)   // true
+// console.log(+true)  //  1
+// //console.log(true+) // error
+// console.log(+"")   // 0
+// //console.log(""+)   // error
+
+
+let gameCounter=100
+let inc=gameCounter++
+console.log(gameCounter)  //101
+console.log(inc)  // 100
