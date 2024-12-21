1551be17a961e93afb3e216e3ef19d3e6daa6c98
AkshayRoyak <work.akshayroy@gmail.com>
2024-12-21 05:27:07 +0000

    changes

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,99 @@
+const name = "Akshay"
+let age = 20
+//console.log(name + age + "Roy")
+console.log(`Hello, My name is ${name} and my age is ${age}`)
+
+const myName = new String('Akshay-kumar-roy')
+//console.log(myName[0])
+//console.log(myName[1])
+//console.log(myName[2])
+//console.log(myName.__proto__)
+//console.log(myName.length)
+//console.log(myName.toUpperCase())
+//console.log(myName.toLowerCase())
+//console.log(myName)   // no change in original string
+//console.log(myName.charAt(4))
+//console.log(myName.indexOf("a"))
+
+// const newstring = myName.substring(0,4)
+// console.log(newstring)
+
+// const anotherString = newstring.slice(-5,1)
+// console.log(anotherString)
+
+// const newStringOne = "    Akshay     "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+// trim works only in whitespace but not in line terminator(\n)
+
+
+const url = "https://akshay.com/akshay%20roy"
+ console.log(url.replace('%20','-'))
+ console.log(url.includes("akshay"))
+
+console.log(myName.split('-'))
+
+const heart = '\u2764'; // Unicode for "❤"
+console.log(heart); // Output: ❤
+
+
+
+// [[Prototype]]: String
+//f= functions
+// anchor : ƒ anchor()
+// at: ƒ at()
+// big: ƒ big()
+// blink :  ƒ blink()
+// bold :  ƒ bold()
+// charAt :  ƒ charAt()
+// charCodeAt :  ƒ charCodeAt()
+// codePointAt :  ƒ codePointAt()
+// concat :  ƒ concat()
+// constructor :  ƒ String()
+// endsWith :  ƒ endsWith()
+// fixed :  ƒ fixed()
+// fontcolor :  ƒ fontcolor()
+// fontsize :  ƒ fontsize()
+// includes :  ƒ includes()
+// indexOf :  ƒ indexOf()
+// isWellFormed :  ƒ isWellFormed()
+// italics :  ƒ italics()
+// lastIndexOf :  ƒ lastIndexOf()
+
+
+// length :  0
+
+// link :  ƒ link()
+// localeCompare :  ƒ localeCompare()
+// match :  ƒ match()
+// matchAll :  ƒ matchAll()
+// normalize :  ƒ normalize()
+// padEnd :  ƒ padEnd()
+// padStart :  ƒ padStart()
+// repeat :  ƒ repeat()
+// replace :  ƒ replace()
+// replaceAll :  ƒ replaceAll()
+// search :  ƒ search()
+// slice :  ƒ slice()
+// small :  ƒ small()
+// split :  ƒ split()
+// startsWith :  ƒ startsWith()
+// strike :  ƒ strike()
+// sub :  ƒ sub()
+// substr :  ƒ substr()
+// substring :  ƒ substring()
+// sup :  ƒ sup()
+// toLocaleLowerCase :  ƒ toLocaleLowerCase()
+// toLocaleUpperCase :  ƒ toLocaleUpperCase()
+// toLowerCase :  ƒ toLowerCase()
+// toString :ƒ toString()
+// toUpperCase :  ƒ toUpperCase()
+// toWellFormed :  ƒ toWellFormed()
+// trim :  ƒ trim()
+// trimEnd :  ƒ trimEnd()
+// trimLeft :  ƒ trimStart()
+// trimRight :  ƒ trimEnd()
+// trimStart :  ƒ trimStart()
+// valueOf :  ƒ valueOf()
+// Symbol(Symbol.iterator) : ƒ [Symbol.iterator]()
+// [[Prototype]] :  Object
