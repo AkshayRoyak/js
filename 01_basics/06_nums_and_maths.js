5958b0ffe69a0ed8ad89f46f4c255ccd4fa1c45d
AkshayRoyak <work.akshayroy@gmail.com>
2024-12-21 06:25:21 +0000

    NEW

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,32 @@
+// ++++++++++++ NUMBERS +++++++++
+
+// const score = 400
+// console.log(score)
+// // specifically number
+
+// const balance = new Number(100)
+// console.log(balance)
+// console.log(typeof balance)  // object
+// console.log(typeof score)  // number
+
+// let a=console.log(balance.toString().length)
+// console.log(typeof a) 
+
+// const balance = new Number(100)
+// console.log(balance.toFixed(2))
+
+// const anotherNumber = 23.8966
+// console.log(anotherNumber.toPrecision(3))  // 23.9
+// console.log(anotherNumber.toPrecision(2))  // 24
+// console.log(anotherNumber.toPrecision(1))  // 2e+1
+
+// const hundreds = 100000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-In'))
+// console.log(Number.MAX_VALUE); // 1.7976931348623157e+308
+// console.log(Number.MIN_VALUE); // 5e-324
+
+
+
+// ++++++++++++++  MATHS +++++++++
+
